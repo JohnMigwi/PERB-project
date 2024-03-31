3589a352c3917c7f3f229e4b3027b7d85654f331
JohnMigwi <johnmigwi101@gmail.com>
2024-03-31 19:24:02 +0300

    run server on PORT 5000

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+//middleware
+app.use(cors());
+//getting request from user
+app.use(express.json());
 
 
 const PORT = 5000;
-
 app.listen(PORT, ()=>{
     console.log("server has started on port", PORT);
 })
