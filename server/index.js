64570ab6b54857bfb287c1b8d768d6fe76cf5fc8
JohnMigwi <johnmigwi101@gmail.com>
2024-03-31 20:08:55 +0300

    connection to POSTGRES

--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const pool = require('./db')
 
 //middleware
 app.use(cors());
 //getting request from user
 app.use(express.json());
 
+//ROUTES
 
+//create a todo
+
+
+//get all todo
+
+
+//get a todo
+
+
+//Update a todo
+
+
+//Delete a todo
 const PORT = 5000;
 app.listen(PORT, ()=>{
     console.log("server has started on port", PORT);
