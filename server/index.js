9ace19d806d59de5f2c29724d4fa41bec54d7dc2
JohnMigwi <johnmigwi101@gmail.com>
2024-04-05 11:21:30 +0300

    Revert "get a todo"

--- server/index.js
+++ server/index.js
@@ -102,7 +102,7 @@ app.use(express.json());
 //ROUTES
 
 //create a todo
-app.post('/todo', async(req,res) =>{
+app.post('/todos', async(req,res) =>{
     try {
         const description = req.body;
         const newtodo = await pool.query("INSERT INTO todo (description) VALUES($1)",
@@ -115,35 +115,11 @@ app.post('/todo', async(req,res) =>{
 
 });
 
+
 //get all todo
-app.post('/todos', async(req, res)=>{
-    try {
-        const alltodo = await pool.query("SELECT * FROM todo");
-       const response= req.json(alltodo.rows);
-       console.log(response)
-    } catch (error) {
-        console.log(error.message);
-    }
-    
-});
 
 
 //get a todo
-app.put('/todos/:id', async(req, res) => {
-    try {
-        const { id } = req.params;
-        const todo = await pool.query("SELECT * FORM todo WHERE todo_id = $1", 
-        [id]);
-        if (todo.rows.length === 0) {
-            return res.status(404).json({ message : "Todo not found"});
-        }
-        res.json(todo.row[id]);
-        
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).send("Server error");
-    }
-})
 
 
 //Update a todo
