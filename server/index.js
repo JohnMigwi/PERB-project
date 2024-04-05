f8e53223a6f70b652706c60c260d81151e22a5de
JohnMigwi <johnmigwi101@gmail.com>
2024-04-05 11:17:41 +0300

    Revert "all endpoints test success ready to be used"
    This reverts commit 7fd31b355596cbc09b0c6a18dd80200222f951ef.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const pool = require('./db');
+const pool = require('./db')
 
+<<<<<<< HEAD
 
 const PORT = 5000;
 app.listen(PORT, () => {
@@ -93,79 +94,75 @@ const pool = require('./db');
 >>>>>>> parent of 95c00c0 (put)
 
 // Middleware
+=======
+//middleware
+>>>>>>> parent of 7fd31b3 (all endpoints test success ready to be used)
 app.use(cors());
+//getting request from user
 app.use(express.json());
 
-// Routes
+//ROUTES
 
-// Create a todo
-app.post('/todos', async (req, res) => {
+//create a todo
+app.post('/todo', async(req,res) =>{
     try {
-        const { description } = req.body;
-        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description]);
-        res.json(newTodo.rows[0]);
+        const description = req.body;
+        const newtodo = await pool.query("INSERT INTO todo (description) VALUES($1)",
+        [description]);
+        res.json(newtodo);
+        
     } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Server Error");
+        console.log(error.message);
     }
+
 });
 
-// Get all todos
-app.get('/todos', async (req, res) => {
+//get all todo
+app.post('/todos', async(req, res)=>{
     try {
-        const allTodos = await pool.query("SELECT * FROM todo");
-        res.json(allTodos.rows);
+        const alltodo = await pool.query("SELECT * FROM todo");
+       const response= req.json(alltodo.rows);
+       console.log(response)
     } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Server Error");
+        console.log(error.message);
     }
+    
 });
 
-// Get a todo
-app.get('/todos/:id', async (req, res) => {
+
+//get a todo
+app.put('/todos/:id', async(req, res) => {
     try {
         const { id } = req.params;
-        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+        const todo = await pool.query("SELECT * FORM todo WHERE todo_id = $1", 
+        [id]);
         if (todo.rows.length === 0) {
-            return res.status(404).json({ message: "Todo not found" });
+            return res.status(404).json({ message : "Todo not found"});
         }
-        res.json(todo.rows[0]);
+        res.json(todo.row[id]);
+        
     } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Server Error");
+        console.log(error.message)
+        res.status(500).send("Server error");
     }
-});
+})
 
-// Update a todo
-app.put('/todos/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { description } = req.body;
-        const updatedTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2 RETURNING *", [description, id]);
-        if (updatedTodo.rows.length === 0) {
-            return res.status(404).json({ message: "Todo not found" });
-        }
-        res.json(updatedTodo.rows[0]);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Server Error");
-    }
-});
 
-// Delete a todo
-app.delete('/todos/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const deletedTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1 RETURNING *", [id]);
-        if (deletedTodo.rows.length === 0) {
-            return res.status(404).json({ message: "Todo not found" });
-        }
-        res.json({ message: "Todo deleted" });
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Server Error");
-    }
-});
+//Update a todo
+try {
+    
+} catch (error) {
+    
+}
 
+<<<<<<< HEAD
 // Start the server
 
+=======
+
+//Delete a todo
+const PORT = 5000;
+app.listen(PORT, ()=>{
+    console.log("server has started on port", PORT);
+})
+>>>>>>> parent of 7fd31b3 (all endpoints test success ready to be used)
