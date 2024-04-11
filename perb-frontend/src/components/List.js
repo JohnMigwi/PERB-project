630b1d81f90a2f95c2f4410a90c5cda70ec04bcc
JohnMigwi <johnmigwi101@gmail.com>
2024-04-11 13:21:04 +0300

    PUT update

--- perb-frontend/src/components/List.js
+++ perb-frontend/src/components/List.js
@@ -1,6 +1,7 @@
 import React, {Fragment, useEffect, useState} from 'react'
+import Edit from './Edit';
 
-function List() {
+function List(editTodo) {
 
   const [todos, setTodos] = useState([]);
 
@@ -57,7 +58,7 @@ useEffect (() =>{
       {todos.map( todo =>(
         <tr key={todo.todo_id}>
         <td>{todo.description}</td>
-        <td>Edit</td>
+        <td><Edit todo={todo}/></td>
         <td>
         <button className='btn btn-danger' onClick={ () => deleteTodo(todo.todo_id)}>Delete</button>
 
