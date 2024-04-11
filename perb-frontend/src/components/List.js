875758aab5eefbbba0b65bf1c51e5880664e931e
JohnMigwi <johnmigwi101@gmail.com>
2024-04-11 12:25:32 +0300

    delete component

--- perb-frontend/src/components/List.js
+++ perb-frontend/src/components/List.js
@@ -1,8 +1,72 @@
-import React from 'react'
+import React, {Fragment, useEffect, useState} from 'react'
 
 function List() {
+
+  const [todos, setTodos] = useState([]);
+
+
+  async function deleteTodo(id){
+    try {
+      const response = await fetch(`http://localhost:5000/todo/${id}`,{
+        method:"DELETE"
+      });
+      if (response.ok) {
+        await getTodos();
+      } else {
+        throw new Error('Failed to delete todo');
+      }
+      
+    } catch (error) {
+      console.log(error.message);
+    }
+    
+  }
+
+
+
+  async function getTodos(){
+      try {
+        const response = await fetch ("http://localhost:5000/todos");
+        const data = await response.json();
+        setTodos(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+     
+  };
+
+ 
+
+useEffect (() =>{
+  getTodos();
+},[])
+
+
+
   return (
-    <div>List</div>
+    <Fragment>
+    <table class="table mt-5 text-center">
+    <thead>
+      <tr>
+        <th>Description</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+      {todos.map( todo =>(
+        <tr key={todo.todo_id}>
+        <td>{todo.description}</td>
+        <td>Edit</td>
+        <td>
+        <button className='btn btn-danger' onClick={ () => deleteTodo(todo.todo_id)}>Delete</button>
+
+        </td>
+        </tr>
+      ))}
+    </tbody>
+     </table>
+    </Fragment>
   )
 }
 
